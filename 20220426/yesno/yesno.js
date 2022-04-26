dc41c84b7705a097c968fb41535a2e9441c7c5d5
Meija Lohiniva <mlohiniva@hotmail.com>
2022-04-26 19:51:01 +0300

    YesNo added

---
+++ 20220426/yesno/yesno.js
@@ -0,0 +1,8 @@
+fetch('https://yesno.wtf/api')
+    .then(response => response.json())
+    .then(
+        data => {
+            document.getElementById('print-here').innerHTML = "<h2>" + data.answer + "</h2>";
+            document.getElementById('print-here').innerHTML += "<img src=\"" + data.image + "\"/>";
+        }
+    );
