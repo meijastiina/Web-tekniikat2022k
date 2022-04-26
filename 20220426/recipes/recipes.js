b8c7075fb133709e248c0659262d330be73a4ca9
Meija Lohiniva <mlohiniva@hotmail.com>
2022-04-26 19:11:29 +0300

    Välisave

--- 20220426/recipes/recipes.js
+++ 20220426/recipes/recipes.js
@@ -32,11 +32,32 @@ let recipes = [
 // Hae tulostuselementti muuttujaan
 let element = document.getElementById("printHere");
 // Looppaa reseptit läpi ja tulosta sivulle
-let html = "<table>";
+let html = "<table><tr><th>Name</th><th>Cooking Time</th><th>Ingredients</th></tr>";
 recipes.forEach(recipe => {
     html += "<tr>";
     html += "<td>" + recipe.name + "</td>";
+    html += "<td>" + recipe.cookingTime + "</td>";
+    html += "<td>" + recipe.ingredients + "</td>";
     html += "</tr>";
 });
 html += "</table>";
 element.innerHTML = html;
+
+// Vaihtoehtoinen tapa
+// Luo uusi table-elementti
+let table = document.createElement('table');
+// Looppaa array läpi
+for ( let i = 0; i < recipes.length; i++ ) {
+    // Luo uusi tr-elementti
+    let tr = document.createElement('tr');
+    // Luo uusi td-elementti
+    let td = document.createElement('td');
+    // Laita reseptin nimi solulle
+    td.innerHTML = recipes[i].name;
+    // Lisää td trlle
+    tr.appendChild(td);
+    // Lisää tr tablelle
+    table.appendChild(tr);
+}
+// Lisää table domiin
+element.appendChild(table);
